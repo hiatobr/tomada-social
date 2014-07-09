6ce2d451dd8e3768bb74a11b3edfd401d8ddf1eb
Ederson Brilhante <contato@edersonbrilhante.com.br>
2014-07-09 00:42:19 -0300

    angularjs

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tomadaSocialApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tomadaSocialApp
+ */
+angular.module('tomadaSocialApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
